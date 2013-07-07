91af90faa9069df094cf6a3da422f7a5e6d86746
palash <palash@palash-Lenovo-G580.(none)>
2013-07-07 12:58:45 +0530

    initial commit

---
+++ application/controllers/main.php
@@ -0,0 +1,342 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Main extends CI_Controller {
+ 
+    function __construct()
+    {
+        parent::__construct();
+ 
+        /* Standard Libraries of codeigniter are required */
+        $this->load->database();
+        $this->load->helper('url');
+        /* ------------------ */ 
+        $config['upload_path'] = './assets/uploads/cv';
+        $config['allowed_types'] = 'gif|jpg|png|pdf|doc|docx|txt';
+        $this->load->library('upload', $config);
+        $this->load->library('grocery_CRUD');
+		//$this->load->library('email');
+		//$config['protocol'] = 'sendmail';
+		//$config['charset'] = 'utf-8';
+		//$config['wordwrap'] = TRUE;
+		//$config['mailtype'] = 'html';
+		//$this->email->initialize($this->config);
+
+		//$this->load->model('Cms');
+ 
+    }
+    public function _renderView($page,$data) {
+                
+/*                $data['featured_menu'] = $this->Cms->get_featured_menu();
+                $data['news'] = $this->Cms->get_news_list(1);
+                $data['whoweare_links']=$this->Cms->get_page_basedonCatId('aboutus');
+                
+*/             // $data['top_menu']=$this->Cms->get_topmenu(); 
+                //$data['product_cat']=$this->Cms->get_product_cat();
+//                echo "<pre>";
+//                print_r($data['top_menu']);
+//		echo "</pre>";
+//		die();
+//                $this->load->view('fe/common/header.php',$data);
+                $this->load->view($page.'.php',$data);
+//   $this->load->view('fe/common/footer.php',$data);
+    }
+
+    public function _renderViewContact($page,$data) {
+                
+                //$data['featured_menu'] = $this->Cms->get_featured_menu();
+                //$data['news'] = $this->Cms->get_news_list(1);
+                //$data['whoweare_links']=$this->Cms->get_page_basedonCatId('aboutus');
+                
+               // echo "hiii";
+                //$data['top_menu']=$this->Cms->get_topmenu();
+//                $data['product_cat']=$this->Cms->get_product_cat();
+//                $this->load->view('fe/common/product_gallery_header.php',$data);
+              $this->load->view('fe/'.$page.'.php',$data);
+      //          $this->load->view('fe/common/footer.php',$data);
+    }
+
+    public function index()
+    {
+               // $data['pageDetail'] = $this->Cms->get_page_content(8);
+               // $data['feturedProduct'] = $this->Cms->get_fetured_product();
+//                echo "<pre>";
+//                print_r($data['feturedProduct']);
+//		echo "</pre>";
+//		die();
+		
+        $this->_renderView('index',$data = NULL);
+        
+        
+
+    }
+
+    public function page($id)
+    {
+		$data['pageDetail'] = $this->Cms->get_page_content($id);
+
+//		echo "<pre>";
+//              print_r($data['pageDetail']);
+//		echo "</pre>";
+//		die();
+                switch ($data['pageDetail']->type) {
+                    case 'content':$this->_renderView('inner_page',$data);
+                        break;
+                    case 'product':$this->_renderView('inner_page',$data);
+                        break;
+                     case 'contact':$this->_renderViewContact('contact_us',$data);
+                        break;
+                     case 'enquiry':$this->_renderViewContact('enquiry',$data);
+                        break;
+                    default:
+                      $this->_renderView('inner_page',$data);
+                        break;
+                }
+ 	    
+    }
+
+    public function gallery()
+    {
+		$data['galleryContent'] = $this->Cms->get_gallery_content_all();
+                $this->_renderView('gallery',$data);
+    }
+
+    public function product()
+    {
+		$data['productContent'] = $this->Cms->get_recruitment_content_all();
+                $this->_renderView('product',$data);
+    }	
+   
+    public function recruitment_details($id)
+    {
+		$data['recruitmentContent'] = $this->Cms->get_recruitment_content($id);
+                $this->_renderView('recruitment_details',$data);
+		
+    }
+    
+    public function recruitment_apply($id,$message = null)
+    {
+		$data['recruitmentContent'] = $this->Cms->get_recruitment_content($id);
+//                echo "<pre>";
+//                print_r($data['recruitmentContent']);
+//                echo "</pre>";
+//                die();
+                $data['message'] = $message ;
+                $this->_renderView('recruitment_apply',$data);
+		
+    }	
+
+    //for categories
+    public function categories($catName) {
+        
+        $data['categories_items'] =  $this->Cms->get_page_basedonCatId($catName);
+        $this->_renderView('categories',$data);
+    }
+	
+    public function contact_us()
+    {
+				
+                        //$data['contact_us_data']=$this->Cms->get_page_content(19);
+		       
+                        $data='';
+                        $this->_renderViewContact('contact_us',$data);
+    }
+ 	
+    public function contactus_email()
+    {
+                    try
+                    {
+                            unset($_POST['action']);
+                            $posted=array();
+                            $posted["name"]  	= trim($this->input->post("name"));
+                            $posted["email"]  	= trim($this->input->post("email"));
+                            $posted["mobile"]  	= trim($this->input->post("mobile"));
+                            $posted["address"]  = trim($this->input->post("address"));
+                            $posted["message"]  = trim($this->input->post("message"));
+//                            echo "hello";
+//                            echo "<pre>";
+//                            print_r($posted);
+//                            echo "</pre>";
+//                            die();
+                                                           
+                            // Call model and insert data
+                            //$this->form_validation->set_rules('fname', 'fname', 'trim|required|xss_clean');
+                            //$this->form_validation->set_rules('lname', 'lname', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('name', 'name', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('email', 'email', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('mobile', 'mobile', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('address', 'address', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('message', 'message', 'trim|required|xss_clean');
+                            $this->form_validation->set_message('required', 'Please fill in the fields');
+                            if($this->form_validation->run() == FALSE)/////invalid
+                            {
+                            ////////Display the add form with posted values within it////
+                            $this->data["posted"]=$posted;
+                            }
+                            else
+                            {
+                            // ------------------ email send code start ------------------ //
+
+                            $message='
+                            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+                            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                            <html xmlns="http://www.w3.org/1999/xhtml">
+                            <head></head>
+                            <body>
+                            <table>
+                            <tr><td>Name:</td><td>' .  $posted['name'].'</td></tr>
+                            <tr><td>Email:</td><td>' . $posted["email"] . '</td></tr>
+                            <tr><td>Mobile:</td><td>' . $posted["mobile"] . '</td></tr>
+                            <tr><td>Address:</td><td>' . $posted["address"] . '</td></tr>
+                            <tr><td>Message:</td><td>' . nl2br($posted["message"]) . '</td></tr>
+                            </table>
+                            </body>
+                            </html>
+                            ';
+
+
+                           // $status = $this->email_send($message,'sahani.bunty9@gmail.com',$posted["email"]);
+                            $status = $this->email_send($message,'info@skgroups.org',$posted["email"]);
+
+                            if($status == 'success'){
+                               echo "Thank you for contacting us"; 
+                            } else {
+                               echo "Message sending failed !"; 
+                            }
+
+                            }								
+                    }
+                    catch(Exception $err_obj)
+                    {
+                                    show_error($err_obj->getMessage());
+                    }
+            }
+            
+            
+public function enquiry_email()
+    {
+                    try
+                    {
+                            unset($_POST['action']);
+                            $posted=array();
+                            $posted["f_name"]  	= trim($this->input->post("f_name"));
+                            $posted["l_name"]  	= trim($this->input->post("l_name"));
+                            $posted["email"]  	= trim($this->input->post("email"));
+                            $posted["phone"]  = trim($this->input->post("phone"));
+                            $posted["address"]  = trim($this->input->post("address"));
+                            $posted["address2"]  = trim($this->input->post("address2"));
+                            $posted["address3"]  = trim($this->input->post("address3"));
+                            $posted["city"]  = trim($this->input->post("city"));
+                            $posted["country"]  = trim($this->input->post("country"));
+                            $posted["postcode"]  = trim($this->input->post("postcode"));
+                            $posted["message"]  = trim($this->input->post("message"));
+//                            echo "hello";
+//                            echo "<pre>";
+//                            print_r($posted);
+//                            echo "</pre>";
+//                            die();
+                                                           
+                            // Call model and insert data
+                            //$this->form_validation->set_rules('fname', 'fname', 'trim|required|xss_clean');
+                            //$this->form_validation->set_rules('lname', 'lname', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('f_name', 'f_name', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('l_name', 'l_name', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('email', 'email', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('phone', 'phone', 'trim|required|xss_clean');
+                            $this->form_validation->set_rules('message', 'message', 'trim|required|xss_clean');
+                            $this->form_validation->set_message('required', 'Please fill in the fields');
+                            if($this->form_validation->run() == FALSE)/////invalid
+                            {
+                            ////////Display the add form with posted values within it////
+                            $this->data["posted"]=$posted;
+                            }
+                            else
+                            {
+                            // ------------------ email send code start ------------------ //
+
+                            $message='
+                            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+                            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                            <html xmlns="http://www.w3.org/1999/xhtml">
+                            <head></head>
+                            <body>
+                            <table>
+                            <tr><td>First Name:</td><td>' .  $posted['f_name'].'</td></tr>
+                            <tr><td>Last Name:</td><td>' . $posted["l_name"] . '</td></tr>
+                            <tr><td>Email:</td><td>' . $posted["email"] . '</td></tr>
+                            <tr><td>Phone:</td><td>' . $posted["phone"] . '</td></tr>
+                            <tr><td>Address:</td><td>' . $posted["address"] . '</td></tr>
+                            <tr><td>Address2:</td><td>' .  $posted['address2'].'</td></tr>
+                            <tr><td>Address3:</td><td>' . $posted["address3"] . '</td></tr>
+                            <tr><td>City:</td><td>' . $posted["city"] . '</td></tr>
+                            <tr><td>Country:</td><td>' . $posted["country"] . '</td></tr>
+                            <tr><td>Postcode:</td><td>' . $posted["postcode"] . '</td></tr> 
+                            <tr><td>Message:</td><td>' . nl2br($posted["message"]) . '</td></tr>   
+                            </table>
+                            </body>
+                            </html>
+                            ';
+
+
+                            //$status = $this->email_send($message,'sahani.bunty9@gmail.com',$posted["email"]);
+                            $status = $this->email_send($message,'info@skgroups.org',$posted["email"]);
+
+                            if($status == 'success'){
+                               echo "Thank you for your enquiry"; 
+                            } else {
+                               echo "Message sending failed !"; 
+                            }
+
+                            }								
+                    }
+                    catch(Exception $err_obj)
+                    {
+                                    show_error($err_obj->getMessage());
+                    }
+            }            
+        
+	
+    public function email_send($message,$email_to,$email_from,$filepath = null)
+    {
+    try
+    {
+
+                                    $this->load->library('email');
+                                    $email_setting  = array('mailtype'=>'html');
+                                    $this->email->initialize($email_setting);
+                                    //$email_to    = 'siddharth@satyajittech.com';
+                                    //$email_from  =  $posted["email"];
+                                    $this->email->from($email_from, 'SKGROUP');
+                                    $this->email->to($email_to);
+                                    $this->email->bcc('sahani.bunty9@gmail.com');
+                                    $this->email->subject('SKGROUP :');
+                                    $this->email->message($message);
+                                    if($filepath != NULL){
+                                       $this->email->attach($filepath); 
+                                      // echo $message;
+                                      // echo "test";
+                                      // die;
+                                    }
+
+                                    if($this->email->send())
+                                    {
+                                                 return 'success';
+
+                                    }					
+
+                 // ------------------ email send code end ------------------//
+
+
+
+
+    }
+    catch(Exception $err_obj)
+    {
+                    show_error($err_obj->getMessage());
+    }
+
+    }	
+
+}
+ 
+/* End of file main.php */
+/* Location: ./application/controllers/main.php */
---
+++ application/views/index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sidekick Sports</title>
+
+<link href="<?php echo site_url('css/style.css');?>" rel="stylesheet" type="text/css" media="screen" />
+
+<script type="text/javascript" src="<?php echo site_url('script/jquery-1.js');?>"></script>
+<script type="text/javascript" src="<?php echo site_url('script/banner.js');?>"></script>
+
+<script type="text/javascript">
+$(function() {
+    $(".carouel_area").jCarouselLite({
+        btnNext: ".next",
+        btnPrev: ".prev"
+    });
+});
+
+</script>
+
+<!--######################################################-->
+
+<link rel="stylesheet" href="<?php echo site_url('css/superfish.css')?>">
+  
+  <script>
+    (function() {
+      var jasmineEnv = jasmine.getEnv()
+      jasmineEnv.addReporter(new jasmine.HtmlReporter)
+      window.onload = function() {
+        jasmineEnv.execute()
+      };
+    })();
+  </script>
+
+<!--######################################################-->
+
+</head>
+
+<body>
+<!-- Content section -->
+<div class="wrapper">
+<div class="top_content">
+<div class="top_pan">
+    <div class="logo"><a href="index.html"><img src="<?php echo site_url('images/logo.png')?>" alt="Sidekick Sports" title="Sidekick Sports" /></a></div>
+    
+
+<div class="top_right">
+
+    <ul>
+    <li><a href="#">Home</a><span>|</span></li>
+    <li><a href="#">Sitemap</a><span>|</span></li>
+    <li><a href="#">Contacts</a></li>
+    </ul>
+    <div class="clear"></div>
+    <div class="top_link">
+    <h4>Follow us:</h4>
+    <ul>
+    <li><a href="#"><img src="<?php echo site_url('images/google.png')?>" alt="Google" title="Google" /></a></li>
+    <li><a href="#"><img src="<?php echo site_url('images/twitter.png')?>" alt="Twitter" title="Twitter" /></a></li>
+    <li><a href="#"><img src="<?php echo site_url('images/facebook.png')?>" alt="facebook" title="Facebook" /></a></li>
+    <li><a href="#"><img src="<?php echo site_url('images/linkedin.png')?>" title="Linkedin" /></a></li>
+    </ul>
+    </div>
+    
+    </div>
+
+
+
+
+</div>
+</div>
+<div class="menu_section">
+<div class="menu_wrapper">
+  <div id="jasmine_content">
+
+    <ul class="sf-menu" id="example">
+      <li>
+        <a href="#" class="current">Home</a>
+  
+      </li>
+      <li>
+        <a href="#">About Us</a>
+      </li>
+      <li>
+        <a href="#">Products</a>
+        <ul>
+          <li>
+            <a href="#">Product 1</a>
+           </li> 
+           <li>
+            <a href="#">Product 2</a>
+           </li>  
+           <li>
+            <a href="#">Product 3</a>
+           </li> 
+            
+            
+            
+            </ul>
+          
+       
+    
+
+  <li>
+        <a href="#">gallery</a>
+      </li>
+  <li>
+        <a href="#">Contact Us</a>
+      </li>
+  </ul>
+  
+  
+  </div>
+
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<div class="border"></div>
+
+<div class="container">
+
+<!-- Slide show start -->
+
+<div class="slideshow">
+
+<!-- img_1 -->
+<div class="slides" style="visibility:visible" id="image1">
+    <img src="<?php echo site_url('images/img_1.png') ?>" alt="" />
+</div>
+
+<!-- img_2 -->
+<div class="slides" id="image2">
+<img src="<?php echo site_url('images/img_2.png') ?>" alt="" />
+</div>
+
+<!-- img_3 -->
+<div class="slides" id="image3">
+<img src="<?php echo site_url('images/img_3.png') ?>" alt="" />
+</div>
+
+<!-- img_4 -->
+<div class="slides" id="image4">
+<img src="<?php echo site_url('images/img_4.png') ?>" alt="" />
+</div>
+
+<!-- img_5 -->
+<!--<div class="slides" id="image5">
+<img src="images/img_5.png" alt="" />
+</div>
+-->
+<!-- img_6 -->
+<!--<div class="slides" id="image6">
+<img src="images/img_6.png" alt="" />
+</div>
+-->
+</div>
+
+
+<div class="shadow"></div>
+
+
+<!-- Slide show end -->
+
+
+<div class="clear"></div>    
+    
+
+
+<div class="middle_pan">
+<div class="column three margin">
+    <img src="<?php echo site_url('images/small_img_1.jpg')?>" alt="" />
+</div>
+
+<div class="column three margin">
+	<img src="<?php echo site_url('images/small_img_2.jpg')?>" alt="" />
+</div>
+<div class="column three margin">
+	<img src="<?php echo site_url('images/small_img_3.jpg')?>" alt="" />
+</div>
+
+<div class="clear"></div>
+</div>
+
+
+<div class="content_area">
+<div class="content">
+<div class="content_area_left">
+<h2>About Us</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskt3p publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskt3p publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskt3p publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<a href="#" class="more">...more</a>
+</div>
+
+<div class="content_area_rihgt">
+<h2>Login</h2>
+
+<table width="302" border="0">
+  <tr>
+    <td width="76" height="54"><p>Email:</p></td>
+    <td width="10">&nbsp;</td>
+    <td width="202"><input type="text" class="login" value="" /></td>
+  </tr>
+  <tr>
+    <td height="43"><p>Password:</p></td>
+    <td>&nbsp;</td>
+    <td><input type="text" class="login" value="" /></td>
+  </tr>
+  <tr>
+    <td height="34">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><input type="button" class="login_button" value="" /></td>
+  </tr>
+</table>
+<div class="clear_20"></div>
+<h2>Latest Offers</h2>
+<ul>
+
+<li> djsagasjdghjasgjdgasjgdjasgdgasjgdash djsagasjdghjasgjdgasjgdjasgdgasjgdash </li>
+<li> djsagasjdghjasgjdgasjgdjasgdgasjgdash djsagasjdghjasgjdgasjgdjasgdgasjgdash </li>
+<li> djsagasjdghjasgjdgasjgdjasgdgasjgdash djsagasjdghjasgjdgasjgdjasgdgasjgdash </li>
+<li> djsagasjdghjasgjdgasjgdjasgdgasjgdash djsagasjdghjasgjdgasjgdjasgdgasjgdash </li>
+<li> djsagasjdghjasgjdgasjgdjasgdgasjgdash djsagasjdghjasgjdgasjgdjasgdgasjgdash </li>
+
+</ul>
+
+<a href="#" class="more">...more</a>
+
+</div> 
+</div>
+
+<div class="clear"></div>
+</div>
+
+</div>
+
+<!--footer_section_start-->
+
+<div class="footer">
+<div class="footer_content">
+<div class="footer_logo"></div>
+
+<div class="footer_left">
+<div class="footer_left_1">
+    <ul>
+    <li><a href="">ENQUIRY</a><span>|</span></li> 
+    <li><a href="">PRODUCTS</a><span>|</span></li>
+    <li><a href="">PHOTO GALLERY</a><span>|</span></li>
+    <li><a href="">SITE MAP</a><span>|</span></li>
+    <li><a href="">CONTACT US</a></li>
+    
+    
+    </ul>
+</div>
+<div class="footer_left_2">Copyright © 2013. Sidekick Sports. All rights reserved.</div>
+</div>
+</div>
+</div>
+<!--footer_section_end-->
+
+
+
+</div>
+
+	
+</body>
+</html>
